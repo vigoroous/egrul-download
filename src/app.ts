fe6e0a168370f1ece452d9bfe2ee0ab7ec3063e2
Bogdan Martyshevskiy <vigoroous@gmail.com>
2023-03-19 20:28:12 +0300

    Add: inn validation; Update: Improved input data filtering;

--- src/app.ts
+++ src/app.ts
@@ -1,80 +1,32 @@
-import { chunk } from "lodash";
-import { proxies, setAxiosConfig } from "./config/proxies";
-import { CompanyService } from "./services/company.service";
-import { createReportXslx, delay, readAllLines, readColumnXlsx } from "./utils";
-import { EgrulPullingService } from "./services/egrulPulling.service";
-import { DadataPullingService } from "./services/dadataPulling.service";
-import { EgrulService } from "./services/egrul.service";
+import { createReportXslx } from "./utils";
+import { runDadata, runEgrul, runInput } from "./utils/features";
 
 
 const options = {
-    useInput: false,
+    useInput: true,
     useEgrul: false,
-    useDadata: false,
+    useDadata: true,
     createReport: true,
-    perPage: 20,
-    wait: 300,
 }
 
 const main = async () => {
-    const { createReport, perPage, useDadata, useEgrul, wait, useInput } = options;
+    const { createReport, useDadata, useEgrul, useInput } = options;
 
-    if (useInput) await runInput();
-
-    if (useEgrul) await runEgrul({ wait, perPage });
+    if (useInput) {
+        await runInput({
+            file: { name: "data/Untitled.xlsx", colName: "B" },
+            type: "xlsx"
+        });
+    }
 
     if (useDadata) await runDadata();
 
-    if(createReport) await createReportXslx();
+    if (createReport) await createReportXslx();
 
+    if (useEgrul) await runEgrul({ wait: 300, perPage: 20 });
 
 }
 
 
 main();
 
-const runDadata = async () => {
-    const unhandledDadataCompanys = await CompanyService.getCompanysUnhandledDadata();
-    if (!unhandledDadataCompanys) return;
-
-    for (const { inn } of unhandledDadataCompanys) {
-        await DadataPullingService.getSuggestionsByInn(inn);
-    }
-}
-
-const runInput = async () => {
-    const inputInnArr = readColumnXlsx("data/input.xlsx", "B");
-    const inputInnArr2 = readAllLines("data/input.txt");
-    const inputInnArr3 = inputInnArr.concat(inputInnArr2);
-
-    for (const inn of inputInnArr3.flatMap(line => line.split(" "))) {
-        await CompanyService.upsertCompany(inn);
-    }
-}
-
-
-const runEgrul = async ({ wait, perPage }: { wait: number; perPage: number }) => {
-    const unhandledEgrulCompanys = await CompanyService.getCompanysUnhandledEgrul();
-    if (!unhandledEgrulCompanys) return;
-
-    const egrulNotDownloaded = await EgrulService.getEgrulNotDownloaded();
-    if (!egrulNotDownloaded) return;
-
-    const innArr = egrulNotDownloaded.map((v) => `${v.i} ${v.o}`)
-        .concat(unhandledEgrulCompanys.map((v) => v.inn));
-
-    const preparedInnArr = chunk(innArr, perPage);
-
-    for (const item of preparedInnArr) {
-        const res = await EgrulPullingService.egrulDownload(item, wait, 1);
-
-        if (!res) continue;
-
-        const pagesCount = Math.ceil(res.total / perPage);
-        if (pagesCount > 1) {
-            for (let page = 2; page <= pagesCount; page++) {
-                await EgrulPullingService.egrulDownload(item, wait, page);
-            }
-        }
-    }
-}
---
+++ src/utils/features.ts
@@ -0,0 +1,77 @@
+import { chunk } from "lodash";
+import { proxies, setAxiosConfig } from "../config/proxies";
+import { EgrulPullingService } from "../services/egrulPulling.service";
+import { DadataPullingService } from "../services/dadataPulling.service";
+import { EgrulService } from "../services/egrul.service";
+import { CompanyService } from "../services/company.service";
+import { readAllLines, readColumnXlsx } from ".";
+import { checkINN } from "ru-validation-codes";
+
+export const runDadata = async () => {
+    const unhandledDadataCompanys = await CompanyService.getCompanysUnhandledDadata();
+    if (!unhandledDadataCompanys) return;
+
+    for (const { inn } of unhandledDadataCompanys) {
+        await DadataPullingService.getSuggestionsByInn(inn);
+    }
+}
+
+type RunInputType = {
+    file: {
+        name: string;
+        colName?: string;
+    };
+    type: "xlsx" | "txt";
+}
+
+export const runInput = async ({ file, type }: RunInputType) => {
+
+    let inputArr: string[] = [];
+    switch (type) {
+        case "txt": {
+            const arr = readAllLines(file.name);
+            inputArr = arr;
+            break;
+        }
+        case "xlsx": {
+            const arr = readColumnXlsx(file.name, file.colName ?? "B");
+            inputArr = arr;
+            break;
+        }
+    }
+
+    const preparedInnArr = inputArr.flatMap(line => line.split(/[ ;]/g));
+
+    for (const inn of preparedInnArr) {
+        if (checkINN(inn)) {
+            await CompanyService.upsertCompany(inn);
+        }
+    }
+}
+
+
+export const runEgrul = async ({ wait, perPage }: { wait: number; perPage: number }) => {
+    const unhandledEgrulCompanys = await CompanyService.getCompanysUnhandledEgrul();
+    if (!unhandledEgrulCompanys) return;
+
+    const egrulNotDownloaded = await EgrulService.getEgrulNotDownloaded();
+    if (!egrulNotDownloaded) return;
+
+    const innArr = egrulNotDownloaded.map((v) => `${v.i} ${v.o}`)
+        .concat(unhandledEgrulCompanys.map((v) => v.inn));
+
+    const preparedInnArr = chunk(innArr, perPage);
+
+    for (const item of preparedInnArr) {
+        const res = await EgrulPullingService.egrulDownload(item, wait, 1);
+
+        if (!res) continue;
+
+        const pagesCount = Math.ceil(res.total / perPage);
+        if (pagesCount > 1) {
+            for (let page = 2; page <= pagesCount; page++) {
+                await EgrulPullingService.egrulDownload(item, wait, page);
+            }
+        }
+    }
+}
